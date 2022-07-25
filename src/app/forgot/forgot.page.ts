5a8bfbc4a3641b8fa4348cd2c6fec1299e141f5d
CloudelLaureta <cloudel.laureta@g.batstate-u.edu.ph>
2022-07-25 12:15:05 +0800

    added an alert

--- src/app/forgot/forgot.page.ts
+++ src/app/forgot/forgot.page.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.page.html',
   styleUrls: ['./forgot.page.scss'],
 })
-export class ForgotPage implements OnInit {
+export class ForgotPage{
+  handlerMessage = '';
+  roleMessage = '';
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor(private alertController: AlertController) {}
+  async reset(){
+    const alert = await this.alertController.create({
+      header: 'REST LINK SENT',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler:() => {this.handlerMessage = 'Alert canceled';}
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
