d1c53904608af6d49534786d6648e3974b183e45
CloudelLaureta <cloudel.laureta@g.batstate-u.edu.ph>
2022-07-25 09:54:06 +0800

    First commit

---
+++ src/app/forgot/forgot.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot',
+  templateUrl: './forgot.page.html',
+  styleUrls: ['./forgot.page.scss'],
+})
+export class ForgotPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
